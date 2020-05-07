19ec02d3f6f3d68633f61ab8e555a089a0913537
hitakarento <hitakarento@gmail.com>
2020-05-07 16:44:48 +0900

    initial commit

---
+++ src/main/java/com/paminedo/PamineDoApplication.java
@@ -0,0 +1,13 @@
+package com.paminedo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PamineDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PamineDoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paminedo/PamineDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paminedo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PamineDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
